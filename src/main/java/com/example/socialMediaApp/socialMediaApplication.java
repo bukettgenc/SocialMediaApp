dfdab91aeddc66ad55ceb3dce6637a2aaca595b9
buketgenc <bktgnc06@gmail.com>
2024-01-15 15:33:03 +0300

    sosyal medya uygulamasi

---
+++ src/main/java/com/example/socialMediaApp/socialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialMediaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class socialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(socialMediaApplication.class, args);
+	}
+
+}

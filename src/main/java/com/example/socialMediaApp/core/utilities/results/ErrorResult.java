bee1e7a9bfe5ba6a289db94ebb34b163ea4af62e
buketgenc <bktgnc06@gmail.com>
2024-01-16 09:40:26 +0300

    result oluşturuldu

---
+++ src/main/java/com/example/socialMediaApp/core/utilities/results/ErrorResult.java
@@ -0,0 +1,14 @@
+package com.example.socialMediaApp.core.utilities.results;
+
+public class ErrorResult extends Result {
+
+	public ErrorResult() {
+		super(true);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ErrorResult(String message) {
+		super(true,message);
+		// TODO Auto-generated constructor stub
+	}
+}
